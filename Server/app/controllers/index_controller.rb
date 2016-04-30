0a53d81310bd17cf6c64cd28fcc14ec0a85f83a6
dhilt <d.alexanov@gmail.com>
2016-04-30 16:57:34 +0300

    index/items back-end ctrl response

--- Server/app/controllers/index_controller.rb
+++ Server/app/controllers/index_controller.rb
@@ -3,8 +3,15 @@ class IndexController < ApplicationController
   # POST /items
   # POST /items.json
   def items
-    tags = params[:tags] || []
-    my = tags
+    unless params[:tags].blank?
+      @items = Item.joins(:tags).where(tags: { id: params[:tags] }).order(:name)
+    end
   end
 
 end
+
+#SELECT "items".* FROM "items"
+#INNER JOIN "items_tags" ON "items_tags"."item_id" = "items"."id"
+#INNER JOIN "tags" ON "tags"."id" = "items_tags"."tag_id"
+#WHERE "tags"."id" IN (7, 8)
+#ORDER BY "items"."name" ASC
