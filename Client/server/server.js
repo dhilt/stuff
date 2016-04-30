992ff5cc5b5fb2e4a07a48738c53839865d58060
dhilt <d.alexanov@gmail.com>
2016-04-30 15:03:35 +0300

    get items by tags nodejs mock response

--- Client/server/server.js
+++ Client/server/server.js
@@ -17,144 +17,158 @@ app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.
 app.use(webpackHotMiddleware(compiler));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + '/dist/index.html')
+	res.sendFile(__dirname + '/dist/index.html')
+});
+
+//-------index-------//
+
+app.post("/api/index", function (req, res) {
+	var tags = req.body;
+	var item, result = [];
+	for (var i = mockData.items.length - 1; i >= 0; i--) {
+		item = mockData.items[i];
+		if(!item.tags) {
+			continue;
+		}
+		for (var j = tags.length - 1; j >= 0; j--) {
+			if (item.tags.indexOf(tags[j]) !== -1) {
+				result.push(item);
+				break;
+			}
+		}
+	}
+	res.send(result);
 });
 
 //-------tags-------//
 
 app.get("/api/tags", function (req, res) {
-    res.send(mockData.tags);
+	res.send(mockData.tags);
 });
 
-var generateNewTag = function (newTag) {
-    if (newTag.id) {
-        return newTag;
-    }
-};
-
 app.post("/api/tags", function (req, res) {
-    var newTag = req.body;
-    var maxId = 0;
-    for (var i = mockData.tags.length - 1; i >= 0; i--) {
-        if (mockData.tags[i].id > maxId) {
-            maxId = mockData.tags[i].id;
-        }
-    }
-    newTag.id = maxId + 1;
-    if (!newTag.description) {
-        newTag.description = "";
-    }
-    mockData.tags.push(newTag);
-    res.send(newTag);
+	var newTag = req.body;
+	var maxId = 0;
+	for (var i = mockData.tags.length - 1; i >= 0; i--) {
+		if (mockData.tags[i].id > maxId) {
+			maxId = mockData.tags[i].id;
+		}
+	}
+	newTag.id = maxId + 1;
+	if (!newTag.description) {
+		newTag.description = "";
+	}
+	mockData.tags.push(newTag);
+	res.send(newTag);
 });
 
 app.put("/api/tags/:id", function (req, res) {
-    var editTag = req.body;
-    for (var i = mockData.tags.length - 1; i >= 0; i--) {
-        if (mockData.tags[i].id === editTag.id) {
-            mockData.tags[i].name = editTag.name;
-            mockData.tags[i].description = editTag.description;
-            res.send(editTag);
-            return;
-        }
-    }
-    res.send(null);
+	var editTag = req.body;
+	for (var i = mockData.tags.length - 1; i >= 0; i--) {
+		if (mockData.tags[i].id === editTag.id) {
+			mockData.tags[i].name = editTag.name;
+			mockData.tags[i].description = editTag.description;
+			res.send(editTag);
+			return;
+		}
+	}
+	res.send(null);
 });
 
 app.delete("/api/tags/:id", function (req, res) {
-    var tagId = req.body.id;
-    var result = {};
-    for (var i = mockData.tags.length - 1; i >= 0; i--) {
-        if (mockData.tags[i].id === tagId) {
-            result.id = tagId;
-            res.send({id: tagId});
-            return;
-        }
-    }
-    res.send(null);
+	var tagId = req.body.id;
+	var result = {};
+	for (var i = mockData.tags.length - 1; i >= 0; i--) {
+		if (mockData.tags[i].id === tagId) {
+			result.id = tagId;
+			res.send({id: tagId});
+			return;
+		}
+	}
+	res.send(null);
 });
 
 //-------items-------//
 
 app.get("/api/items", function (req, res) {
-    var searchString = req.query.searchString;
-    var result = [];
-    for (var i = mockData.items.length - 1; i >= 0; i--) {
-        if (mockData.items[i].name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1) {
-            result.push({
-                id: mockData.items[i].id,
-                name: mockData.items[i].name
-            });
-        }
-    }
-    res.send(result);
+	var searchString = req.query.searchString;
+	var result = [];
+	for (var i = mockData.items.length - 1; i >= 0; i--) {
+		if (mockData.items[i].name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1) {
+			result.push({
+				id: mockData.items[i].id,
+				name: mockData.items[i].name
+			});
+		}
+	}
+	res.send(result);
 });
 
 app.get("/api/items/:id", function (req, res) {
-    var id = parseInt(req.params.id, 10);
-    if(!id) {
-        res.status(400).send('Can not parse id ' + req.params.id);
-        return;
-    }
-    for (var i = mockData.items.length - 1; i >= 0; i--) {
-        if (mockData.items[i].id === id) {
-            res.send(mockData.items[i]);
-            return;
-        }
-    }
-    res.status(400).send('Can not get item with id = ' + req.params.id);
+	var id = parseInt(req.params.id, 10);
+	if (!id) {
+		res.status(400).send('Can not parse id ' + req.params.id);
+		return;
+	}
+	for (var i = mockData.items.length - 1; i >= 0; i--) {
+		if (mockData.items[i].id === id) {
+			res.send(mockData.items[i]);
+			return;
+		}
+	}
+	res.status(400).send('Can not get item with id = ' + req.params.id);
 });
 
 app.post("/api/items", function (req, res) {
-    var newItem = req.body;
-    var maxId = 0;
-    for (var i = mockData.items.length - 1; i >= 0; i--) {
-        if (mockData.items[i].id > maxId) {
-            maxId = mockData.items[i].id;
-        }
-    }
-    newItem.id = maxId + 1;
-    if (!newItem.description) {
-        newItem.description = "";
-    }
-    mockData.items.push(newItem);
-    res.send(newItem);
+	var newItem = req.body;
+	var maxId = 0;
+	for (var i = mockData.items.length - 1; i >= 0; i--) {
+		if (mockData.items[i].id > maxId) {
+			maxId = mockData.items[i].id;
+		}
+	}
+	newItem.id = maxId + 1;
+	if (!newItem.description) {
+		newItem.description = "";
+	}
+	mockData.items.push(newItem);
+	res.send(newItem);
 });
 
 app.put("/api/items/:id", function (req, res) {
-    var editItem = req.body;
-    for (var i = mockData.items.length - 1; i >= 0; i--) {
-        if (mockData.items[i].id === editItem.id) {
-            mockData.items[i].name = editItem.name;
-            mockData.items[i].description = editItem.description;
-            mockData.items[i].tags = editItem.tags;
-            res.send(editItem);
-            return;
-        }
-    }
-    res.send(null);
+	var editItem = req.body;
+	for (var i = mockData.items.length - 1; i >= 0; i--) {
+		if (mockData.items[i].id === editItem.id) {
+			mockData.items[i].name = editItem.name;
+			mockData.items[i].description = editItem.description;
+			mockData.items[i].tags = editItem.tags;
+			res.send(editItem);
+			return;
+		}
+	}
+	res.send(null);
 });
 
 app.delete("/api/items/:id", function (req, res) {
-    var itemId = req.body.id;
-    for (var i = mockData.items.length - 1; i >= 0; i--) {
-        if (mockData.items[i].id === itemId) {
-            result.id = itemId;
-            mockData.items.splice(i, 1);
-            res.send({id: itemId});
-            return;
-        }
-    }
-    res.send(null);
+	var itemId = req.body.id;
+	for (var i = mockData.items.length - 1; i >= 0; i--) {
+		if (mockData.items[i].id === itemId) {
+			result.id = itemId;
+			mockData.items.splice(i, 1);
+			res.send({id: itemId});
+			return;
+		}
+	}
+	res.send(null);
 });
 
 
 //-------start-------//
 
 app.listen(port, function (error) {
-    if (error) {
-        console.error(error);
-    } else {
-        console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
-    }
+	if (error) {
+		console.error(error);
+	} else {
+		console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
+	}
 });
